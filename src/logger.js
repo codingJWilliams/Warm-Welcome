8d0489a7a51b650043202c5c9c2730d7afe8b7be
Jay Williams <course@jaywilliams.me>
2018-04-30 11:19:56 +0100

    Change log transport

--- src/logger.js
+++ src/logger.js
@@ -3,7 +3,6 @@ var strftime = require('strftime');
 
 winston.remove(winston.transports.Console);     // remove the default options
 winston.add(winston.transports.Console, {       // and substitute these
-  timestamp: function () { return strftime('%F %T.%L'); },
   /**formatter: function (options) { // Return string will be passed to winston.
     return options.timestamp() + ' ' + options.level.toUpperCase() + " " + options.message );
   }*/
@@ -11,4 +10,8 @@ winston.add(winston.transports.Console, {       // and substitute these
   level: "silly",
   label: "[warmwelcome]"
 });
+winston.add(winston.transports.File, {
+  filename: "../data/bot.log",
+  level: "silly"
+})
 // add more Winston options if desired
